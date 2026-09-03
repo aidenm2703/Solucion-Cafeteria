3b5719dac35be85f3466e92a42310ea05f0f8b67
urbgmoi98 <moisesurbg98@gmail.com>
2026-09-03 13:23:20 -0600

    stylesCorrection

--- Solucion-Cafeteria/src/main.jsx
+++ Solucion-Cafeteria/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import './Styles/Global.css'   // ← Importa variables + reset + base
+import './Styles/Components.css' // ← Importa utilidades
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
